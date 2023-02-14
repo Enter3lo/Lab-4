e4b16dcd2929f44641192bcd14f45429b7882464
Enter3lo <kth668296@gmail.com>
2023-02-14 11:45:04 +0300

    Добавьте файлы проекта.

---
+++ Лаб 4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаб_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Допустимые операции:  +, -, *, /, 8CC");
+            Console.Write("Первый операнд:  "); int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Операция:  "); string action = Console.ReadLine();
+            char op = action[0];
+            double b;
+            int i;
+            switch(op)
+            {
+                case '+': Console.Write("Второй операнд:  "); b = Convert.ToDouble(Console.ReadLine()); 
+                             Console.WriteLine("Результат:  " + (a + b));
+                                break;
+                case '-': Console.Write("Второй операнд:  "); b = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Результат:  " + (a - b));
+                             break;
+                case '*': Console.Write("Второй операнд:  "); b = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Результат:  " + (a * b));
+                                break;
+                case '/': Console.Write("Второй операнд:  "); b = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Результат:  " + (a / b));
+                                break;
+                case '8': string result = string.Empty;
+                          while(a > 0)
+                          {
+                                i = a % 8;
+                                result = i + result;
+                                a /= 8;
+                          }
+                          Console.WriteLine("Результат:  "+result);
+                          break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
